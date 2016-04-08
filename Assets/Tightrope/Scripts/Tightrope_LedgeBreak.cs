7b8944531e6eaf87eaa329222cb73c00042b42b0
Stefano DAmico <skdamico@gmail.com>
2016-04-07 17:17:31 -0800

    Initial commit with game data

---
+++ Assets/Tightrope/Scripts/Tightrope_LedgeBreak.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tightrope_LedgeBreak : MonoBehaviour {
+
+    private GameObject mainCamera;
+    private RaycastHit hit;
+
+    void Awake() {
+        print("why you no log");
+        mainCamera = GameObject.Find("tr_hmd");
+    }
+
+    void Start() {
+    }
+
+    bool IsPlayerOnLedge() {
+        /*
+        if (Physics.Raycast(mainCamera.transform.position, Vector3.down, out hit)) {
+            print("We hit");
+            return true;//hit.transform.gameObject == this.gameObject;
+        }
+        */
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (this.IsPlayerOnLedge()) {
+            print("Player is on the ledge!");
+        }
+    }
+}
